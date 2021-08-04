10680aef1d4502c44db35220f0a6f4a5145af4a8
santiago <sanntycorrea@gmail.com>
2021-08-04 18:42:06 -0300

    mariana y santy varias modificaciones

--- Js/intro.js
+++ Js/intro.js
@@ -1,3 +1,4 @@
 alert ("Pulse aceptar para entrar a la pagina URBANCHIC");
-var tabla= document.createElement("table");
-tabla
+var tabla=document.createElement("table");
+document.body.appendChild(tabla);
+tabla.text-aling.center;
