029efa98d641ce750b847b2a3fbb5d89c56b594d
lucovati <covati.luciano.epet20@gmail.com>
2021-08-10 19:26:06 -0300

    hola

--- Js/intro.js
+++ Js/intro.js
@@ -2,3 +2,4 @@ alert ("Pulse aceptar para entrar a la pagina URBANCHIC");
 var tabla=document.createElement("table");
 document.body.appendChild(tabla);
 tabla.text-aling.center;
+document.write("hola,como esta");
