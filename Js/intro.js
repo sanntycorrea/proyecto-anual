d5e7177222d95fc5bc1aadc2f55f0509490af3cf
santiago <sanntycorrea@gmail.com>
2021-08-17 14:20:08 -0300

    aprobame widi no seas malo

--- Js/intro.js
+++ Js/intro.js
@@ -1,1 +1,5 @@
 alert ("Pulse aceptar para entrar a la pagina URBANCHIC");
+var Parrafo= Document.createElement("p");
+var Contenido= Document.CreateTextNode ("contenido");
+parrafo.AppendChild("Contenido");
+Document.Body.AppendChild("parrafo");
