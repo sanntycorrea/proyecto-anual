f384f40387a828aa4a17ebaf9bc4ea9dc8204f1d
sanntycorrea <sanntycorrea@gmail.com>
2021-08-27 17:17:19 -0300

    segundo trimestre

--- Js/intro.js
+++ Js/intro.js
@@ -1,5 +1,23 @@
 alert ("Pulse aceptar para entrar a la pagina URBANCHIC");
-var Parrafo= Document.createElement("p");
-var Contenido= Document.CreateTextNode ("contenido");
-parrafo.AppendChild("Contenido");
-Document.Body.AppendChild("parrafo");
+
+
+window.addEventListener("load",function(){
+	document.getElementById("play").addEventListener("click",sonarPajaros);
+	document.getElementById("stop").addEventListener("click",callarPajaros);			
+});
+
+function sonarPajaros(){
+	var sonido = document.createElement("iframe");
+	sonido.setAttribute("src","audio/pajaritos.mp3");
+	document.body.appendChild(sonido);
+	document.getElementById("play").removeEventListener("click",sonarPajaros);
+}
+
+function callarPajaros(){
+	var iframe = document.getElementsByTagName("iframe");
+
+	if (iframe.length > 0){
+		iframe[0].parentNode.removeChild(iframe[0]);
+		document.getElementById("play").addEventListener("click",sonarPajaros);
+	}
+}
