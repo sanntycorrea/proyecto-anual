9a884a871d5da9df0037ea1fe91ddec92fe91652
sanntycorrea <sanntycorrea@gmail.com>
2021-08-31 13:20:15 -0300

    sonido js sol

--- Js/intro.js
+++ Js/intro.js
@@ -1,10 +1,13 @@
 alert ("Pulse aceptar para entrar a la pagina URBANCHIC");
 
 
-const sonidos= document.getElementById('sonidos') ;
+function cargar() {
+    const sonidos= document.getElementById('sonidos') ;
 
  document.addEventListener('keydown', function (evento){
      if (evento.KeyCode ==32) {
          sonidos.innerHTML= '<audio src="sonidos.pajaritos.mp3" autoplay¿></audio>'; 
          }
  }) 
+
+}
