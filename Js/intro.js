d8da2637dcfbf1635319d9465007227848e81753
sanntycorrea <sanntycorrea@gmail.com>
2021-08-31 13:11:39 -0300

    sonido

--- Js/intro.js
+++ Js/intro.js
@@ -1,2 +1,10 @@
 alert ("Pulse aceptar para entrar a la pagina URBANCHIC");
- document.addventListener()
+
+
+const sonidos= document.getElementById('sonidos') ;
+
+ document.addEventListener('keydown', function (evento){
+     if (evento.KeyCode ==32) {
+         sonidos.innerHTML= '<audio src="sonidos.pajaritos.mp3" autoplay¿></audio>'; 
+         }
+ }) 
