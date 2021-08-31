0d54e41988ffc60e5f335c1939fe89b2ba9c1ab1
sanntycorrea <sanntycorrea@gmail.com>
2021-08-31 13:32:16 -0300

    pueba error de sonido

--- Js/intro.js
+++ Js/intro.js
@@ -2,11 +2,11 @@ alert ("Pulse aceptar para entrar a la pagina URBANCHIC");
 
 
 function cargar() {
-    const sonidos= document.getElementById('sonidos') ;
+    const sonidos = document.getElementById('sonidos') ;
 
  document.addEventListener('keydown', function (evento){
      if (evento.KeyCode ==32) {
-         sonidos.innerHTML= '<audio src="sonidos.pajaritos.mp3" autoplay¿></audio>'; 
+         sonidos.innerHTML= '<audio src="sonidos.pajaritos.mp3" autoplay></audio>'; 
          }
  }) 
 
